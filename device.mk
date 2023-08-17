880fa922fc01d884b89e5326e2edcd5dec33775f
Vinoth Jayaram <quic_c_vinoja@quicinc.com>
2023-08-17 18:45:32 +0530

    alioth: Enabled to allow HWC path for WFD
    Change-Id: I7145ce66678901128dfb0c659580f257ce6bb9c1
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -201,7 +201,8 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.display.paneltype=2 \
     ro.vendor.display.sensortype=2 \
     vendor.display.qdcm.mode_combine=1 \
-    vendor.display.defer_fps_frame_count=2
+    vendor.display.defer_fps_frame_count=2 \
+    vendor.display.vds_allow_hwc=1
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=4000 \
