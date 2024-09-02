c4a76948fbd3aa480750ddcc062c0e9b83d8921a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-02 18:35:29 +0200

    alioth: Rename prebuilt audio HAL and libs
    [ peternmuller: Also rename libssrec to libssrec_alioth ]
    
    Change-Id: Ia46a5ba431767795135f058f092aa38999e860ae
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -59,6 +59,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.audio.ring.filter.mask=0 \
     ro.audio.monitorRotation=true \
     ro.config.vc_call_vol_steps=11 \
+    ro.hardware.audio.primary=alioth \
     ro.vendor.audio.enhance.support=false \
     ro.vendor.audio.gain.support=true \
     ro.vendor.audio.karaok.support=true \
