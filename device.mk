1dd836daddc3b1cb5382cf476abae58abd713328
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-09-26 17:20:04 -0300

    alioth: Drop media-legacy component
    Squashed:
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun May 12 10:17:15 2024 +0300
    
        sm6225-common: Drop unused libmm-omxcore
    
        Change-Id: I3bbaa99f9f4541a6e21a8cc9a753a582a1921d01
        Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    
    Author: chrisl7 <wandersonrodriguesf1@gmail.com>
    Date:   Sun Jun 1 23:14:51 2025 +0000
    
        sm6225-common: Move fastcv_headers to media hal
    
        Change-Id: I12be8fcb583960f3878ccdd7bfcc65c0f47b38cc
        Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    
    Author: Sean Du <do4suki@gmail.com>
    Date:   Sun Jun 8 07:17:18 2025 +0800
    
        sm6225-common: Add init.qti.media.rc to common.mk
    
        Needed after "media-prop: Convert to blueprint".
    
        Change-Id: I7ae6d8c9f29c45b9e43b74df81e9d80205f45d2b
        Signed-off-by: Sean Du <do4suki@gmail.com>
    
    Author: chrisl7 <wandersonrodriguesf1@gmail.com>
    Date:   Sun Jun 29 17:09:38 2025 +0000
    
        sm6225-common: Disable debug.stagefright.omx
    
        * In favor of C2
        * https://git.codelinaro.org/clo/la/platform/hardware/qcom/audio-ar/-/blob/LA.VENDOR.13.2.1.r2-10000-DIVAR.QISI15.0/configs/bengal/bengal.mk?ref_type=tags#L441
    
        Change-Id: Idc160b8d68845866bcd6fa167837cf7ee72b4f66
        Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    
    [peternmuller: Adapt to Codec2 for alioth]
    
    Change-Id: I9edf174735482bb3cda308865623b08f15e62891
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Sean Du <do4suki@gmail.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -341,6 +341,35 @@ PRODUCT_VENDOR_PROPERTIES += \
     $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
 endif
 
+# Media
+MSM_VIDC_TARGET_LIST := kona
+
+include hardware/qcom/media/conf_files/$(TARGET_BOARD_PLATFORM)/$(TARGET_BOARD_PLATFORM).mk
+
+PRODUCT_COPY_FILES += \
+    device/qcom/common/vendor/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.2.vendor \
+    init.qti.media.rc \
+    init.qti.media.sh \
+    libavservices_minijail.vendor \
+    libc2dcolorconvert \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libgui_vendor \
+    libjsoncpp.vendor \
+    libstagefrighthw
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    ro.media.recorder-max-base-layer-fps=60
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank=0
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
@@ -468,7 +497,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     gps \
     init \
     keymaster \
-    media \
     overlay \
     perf \
     telephony \
