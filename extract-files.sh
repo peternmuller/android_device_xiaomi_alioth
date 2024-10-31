6b19c66cea5d7493a2d681c1a3d38d895c4ed752
Michael Bestas <mkbestas@lineageos.org>
2024-10-31 15:32:55 +0100

    alioth: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,14 @@ function blob_fixup() {
         vendor/lib64/camera/components/com.mi.node.watermark.so )
             $PATCHELF --add-needed "libpiex_shim.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
