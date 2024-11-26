36bc9e06967a2da3f529c8d8d4ab10bd3c08863a
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-11-26 13:48:00 +0100

    alioth: Build libcurl to vendor
    Change-Id: Icf836ca1a0724e007229ea63a956b7a83ad3db7d
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -217,6 +217,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.height_ratio=1.075 \
     ro.com.google.ime.kb_pad_port_b=8
 
+# GPS
+PRODUCT_PACKAGES += \
+    libcurl.vendor
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
