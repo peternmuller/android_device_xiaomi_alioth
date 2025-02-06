bc1bcc338c0a74f94e61727c0c61845956262b10
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    alioth: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -145,6 +145,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Camera
 PRODUCT_VENDOR_PROPERTIES += \
     camera.disable_zsl_mode=true
