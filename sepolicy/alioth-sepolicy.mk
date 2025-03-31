2bab32c45ad484fd5c0fc7dd0100d6b1cff76b9a
AndroidHQ254 <d.murimi14@gmail.com>
2025-03-31 10:47:38 +0700

    alioth: sepolicy: Label `ro.hardware.chipname` property
    W libc    : Access denied finding property "ro.hardware.chipname"
    
    Change-Id: I3134970f3029b476bcb75dd5ac92ce3ca2722f39
    Signed-off-by: RobertGarciaa <chae0218@naver.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- sepolicy/alioth-sepolicy.mk
+++ sepolicy/alioth-sepolicy.mk
@@ -38,4 +38,5 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/xiaomi/alioth/sepolicy/private/xiaomi/camera \
     device/xiaomi/alioth/sepolicy/private/xiaomi/display \
+    device/xiaomi/alioth/sepolicy/private/xiaomi/hardware \
     device/xiaomi/alioth/sepolicy/private/xiaomi/sensors
